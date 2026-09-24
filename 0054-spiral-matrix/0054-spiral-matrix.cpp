86b3f174d92ff22ccf762d98ce0cf987c6f4355c
Adarsh Kaushik <adarshkaushik844@gmail.com>
2026-09-24 12:55:50 +0530

    Time: 0 ms (100%), Space: 9.2 MB (98.67%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -6,39 +6,40 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        int srow = 0;
-        int erow = n - 1;
-        int scol = 0;
-        int ecol = m - 1;
+        int l = 0;
+        int r = m - 1;
+        int t = 0;
+        int b = n - 1;
 
-        while (srow <= erow && scol <= ecol) {
+        while (t <= b && l <= r) {
 
             // TOP
-            for (int j = scol; j <= ecol; j++) {
-                ans.push_back(matrix[srow][j]);
+            for (int i = l; i <= r; i++) {
+                ans.push_back(matrix[t][i]);
             }
+            t++;
 
             // RIGHT
-            for (int i = srow + 1; i <= erow; i++) {
-                ans.push_back(matrix[i][ecol]);
+            for (int i = t; i <= b; i++) {
+                ans.push_back(matrix[i][r]);
             }
+            r--;
 
             // BOTTOM
-            for (int j = ecol - 1; j >= scol; j--) {
-                if (srow == erow) break;
-                ans.push_back(matrix[erow][j]);
+            if (t <= b) {
+                for (int i = r; i >= l; i--) {
+                    ans.push_back(matrix[b][i]);
+                }
+                b--;
             }
 
             // LEFT
-            for (int i = erow - 1; i >= srow + 1; i--) {
-                if (scol == ecol) break;
-                ans.push_back(matrix[i][scol]);
+            if (l <= r) {
+                for (int i = b; i >= t; i--) {
+                    ans.push_back(matrix[i][l]);
+                }
+                l++;
             }
-
-            srow++;
-            scol++;
-            erow--;
-            ecol--;
         }
 
         return ans;
